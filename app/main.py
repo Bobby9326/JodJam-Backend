4f911418b2d13d9300ca403d32cd8ba049844487
Bobby9326 <oak.siwakorn2547@gmail.com>
2026-05-29 12:57:21 +0700

    Remove logging of DATABASE_URL during application startup

--- app/main.py
+++ app/main.py
@@ -19,7 +19,6 @@ from app.core.logger import logger
 # Lifespan function to run startup/shutdown code
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    logger.info("DATABASE_URL:", settings.DATABASE_URL)
     init_db()
     logger.info("JodJam API started")
     logger.info(
